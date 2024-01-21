9c0637a8230e39a8609f2857257c654e3f5c3fe3
Muskan Sheikh <144351188+mismuskan@users.noreply.github.com>
2024-01-21 11:51:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 //your JS code here. If required.
-const student{
+const student={
 	name:"muskan"
 };
 Object.prototype.getKeys = function () {
   return Object.keys(this);
 };
 
-// Test the getKeys() method on the student object
 const keys = student.getKeys();
-console.log(keys); // Output: ["name"]
+console.log(keys); 
